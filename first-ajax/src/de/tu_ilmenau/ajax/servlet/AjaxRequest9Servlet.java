715c3c2afab86c5dd42faf6fc6b837460cd25655
Binbin Luo <luobinb7@gmail.com>
2023-04-13 19:10:40 +0200

    ajax request encapsulation in jQuery

---
+++ first-ajax/src/de/tu_ilmenau/ajax/servlet/AjaxRequest9Servlet.java
@@ -0,0 +1,31 @@
+package de.tu_ilmenau.ajax.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Author : Binbin Luo
+ * Date : 13.04.2023
+ */
+@WebServlet("/ajaxrequest9")
+public class AjaxRequest9Servlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+//        out.print("<font color='red'>hello ajax!</font>");
+        request.setCharacterEncoding("UTF-8");
+        String username = request.getParameter("username");
+//        out.print("<font color='red'>" + username + "</font>");
+        // 响应一个json {"username":"bubu"}
+        out.print("{\"username\":\"" + username + "\"}");
+    }
+}
--- first-ajax/web/js/jQuery_1.0.js
+++ first-ajax/web/js/jQuery_1.0.js
@@ -5,49 +5,62 @@
 * 3. 想到返回的是一个新的jQuery()对象
 * */
 function jQuery(selector) {
-    // selector可能是#id,也可以是其它选择器，例如.class
-
-    //
     if (typeof selector == "string") {
         if (selector.charAt(0) == '#') {
-            // selector是一个id选择器 因为id是#id开始的
-            // 去掉var作为全局变量，可以在整个页面中使用
-            // 这里就是创造了一个全局变量，用来代替document.getElementById(xxx)
-            domObj = document.getElementById(selector.substring(1)); // 从第二位开始截string
-            // 利用新的jQuery对象去使用.html方法
+            domObj = document.getElementById(selector.substring(1));
             return new jQuery();
         }
     }
-
-    // 页面加载完毕之后，加载函数,如果传入的是函数的话
     if (typeof selector == "function") {
-        // 当这个选择器是一个函数的时候，使它在页面加载完成的时候执行
         window.onload = selector;
     }
-
-    // 在jQuery类中定义一个html()函数，用来代替 domObj.innerHTML = "xxx"
-    // 这是js在类中定义实例方法
     this.html = function (htmlStr) {
         domObj.innerHTML = htmlStr;
     }
-
-    // 定义一个click函数，传入的是一个function，代替 domObj.onclick = function () {...}
     this.click = function (func) {
         domObj.onclick = func;
     }
-
-    // 定义获取元素value的function
     this.val = function (v) {
         if (v != undefined) {
-            // 带有参数的时候是修改某个id中的value
             domObj.value = v;
         } else {
             return domObj.value;
+        }
+    }
+    // 发送ajax的函数
+    // 考虑到请求方式，异步/同步，提交的数据data，url都是动态的
+    jQuery.ajax = function (jsonArgs) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    // 假设服务器返回的都是json
+                    var jsonObj = JSON.parse(this.responseText);
+                    // 处理json
+                    // 调用函数
+                    jsonArgs.success(jsonObj);
+                } else {
+                    alert(xhr.status);
+                }
+            }
+        }
+        // 强制全部转换成为大写
+        if (jsonArgs.method.toUpperCase() == "POST") {
+
+            xhr.open("POST", jsonArgs.url, jsonArgs.async);
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhr.send(jsonArgs.data);
+
+        }
+
+        if (jsonArgs.method.toUpperCase() == "GET") {
+            xhr.open("GET", jsonArgs.url + "?" + jsonArgs.data, jsonArgs.async);
+            xhr.send();
         }
 
 
     }
 }
 
-// 将jQuery又用一个标识符来表示
 $ = jQuery;
+new jQuery();
